1dcdbaf3a6e5be3333ba109c94cff4d17f49deff
Chris Lee <chreestopherlee@gmail.com>
2021-11-27 16:02:49 -0500

    Made some tables to figure out what to display.. Mariah is Mariah

--- scripts/project.R
+++ scripts/project.R
@@ -18,7 +18,8 @@ dat$Date <- as.Date(dat$Date, "%Y-%m-%d")
 
 glimpse(dat)
 
-dat20 <- dat %>% filter(Date >= ("2020-01-01"))
+dat20 <- dat %>% filter(Date >= ("2020-01-01")) %>%
+    mutate(Streams = as.numeric(gsub(",", "", Streams)))
 
 glimpse(dat20)
 
@@ -37,3 +38,24 @@ dat20g %>% ggplot(aes(x = Date, y = n, fill = Christmas)) +
     geom_bar(stat = "identity", position = "fill") +
     geom_text(aes(label = paste0(sprintf("%1.1f", percent * 100), "%")),
     position = position_fill(vjust = 0.5), colour = "white", size = 1)
+
+dat20 %>% group_by(Singer) %>%
+    summarise(TotalStreams = sum(Streams)) %>%
+    arrange(desc(TotalStreams))
+
+dat20 %>% group_by(Christmas, Date) %>%
+    summarise(TotalStreams = sum(Streams)) %>%
+    arrange(desc(TotalStreams))
+
+dat20 %>% group_by(Date) %>%
+    summarise(TotalStreams = sum(Streams)) %>%
+    arrange(desc(TotalStreams)) %>%
+    geom_point(aes(x = Date, y = TotalStreams))
+
+dat20 %>%
+    filter(Rank ==1) %>%
+    count(Song.Title, Singer, sort = TRUE)
+
+dat20 %>%
+    filter(Christmas == 'Y', Rank == 1) %>%
+    count(Song.Title, Singer, sort = TRUE)
