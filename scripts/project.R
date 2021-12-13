549baed72d145d6ac1ef4cb62c71a600a3c5fbef
Chris Lee <chreestopherlee@gmail.com>
2021-12-13 17:38:57 -0500

    Done with Project 2!  Very pleased

--- scripts/project.R
+++ scripts/project.R
@@ -27,12 +27,39 @@ dat20g <- dat20 %>% group_by(Date, Christmas) %>%
     tally() %>%
     mutate(percent = n / sum(n))
 
-glimpse(dat20g)
+dat20 %>%
+    group_by(Date) %>%
+    filter(Christmas == "Y") %>%
+    summarise(TopRank = min(Rank), Date = Date) %>%
+    ggplot(aes(Date, reorder(TopRank, desc(TopRank)))) +
+    geom_point() +
+    theme_bw() +
+    labs(title = "Highest Christmas Song Rank",
+        x = "Date", y = "Rank")
+
+ggsave("documents/xmasrank.png", plot = last_plot())
+
+dat20 %>%
+    group_by(Date) %>%
+    filter(Christmas == "Y", Singer == "Mariah Carey") %>%
+    summarise(TopRank = min(Rank), Date = Date) %>%
+    ggplot(aes(Date, reorder(TopRank, desc(TopRank)))) +
+    geom_point() +
+    theme_bw() +
+    labs(title = "Highest Mariah Carey Song Rank",
+        x = "Date", y = "Rank")
+
+ggsave("documents/mariahrank.png", plot = last_plot())
+
 
 dat20g %>% ggplot(aes(x = Date, y = n, fill = Christmas)) +
     geom_bar(stat = "identity", position = "fill") +
-    geom_text(aes(label = paste0(sprintf("%1.1f", percent * 100), "%")),
-    position = position_fill(vjust = 0.5), colour = "white", size = 1)
+    scale_y_continuous(labels = scales::percent) +
+    scale_fill_manual(values = c("#d1b5b5", "#438b70")) +
+    labs(title = "Percent of Christmas songs in top 200", x = "Date", y = "% of Top 200 Songs", fill = "Christmas Song")
+
+ggsave("documents/xmaspercentot.png", plot = last_plot())
+
 
 dat20 %>% group_by(Singer) %>%
     summarise(TotalStreams = sum(Streams)) %>%
@@ -45,6 +72,7 @@ dat20 %>% group_by(Christmas, Date) %>%
 dat20 %>% group_by(Date) %>%
     summarise(TotalStreams = sum(Streams)) %>%
     arrange(desc(TotalStreams)) %>%
+    ggplot() +
     geom_point(aes(x = Date, y = TotalStreams))
 
 dat20 %>%
@@ -52,7 +80,7 @@ dat20 %>%
     count(Song.Title, Singer, sort = TRUE)
 
 dat20 %>%
-    filter(Christmas == 'Y', Rank == 1) %>%
+    filter(Christmas == "Y", Rank == 1) %>%
     count(Song.Title, Singer, sort = TRUE)
 
 dat20 %>% group_by(Singer) %>%
@@ -63,8 +91,7 @@ dat20 %>% group_by(Singer) %>%
 
 dat20 %>% group_by(Singer) %>%
     summarise(Christmas = Christmas,
-    appearances = n()
-    ) %>%
+    appearances = n()) %>%
     distinct()
 
 #Test
@@ -77,6 +104,17 @@ dat20 %>% group_by(Singer) %>%
     summarise(appearances = n(),
         num1_appearances = sum(Rank ==1),
         distinctsongson100 = n_distinct(Song.Title),
-        Christmasartist = Christmas) %>%
+        TotalStreams = sum(Streams),
+        Christmasartist = max(Christmas)) %>%
         distinct() %>%
-    arrange(desc(appearances))
+    arrange(desc(TotalStreams)) %>%
+    slice(1:10) %>%
+    ggplot(aes(x = reorder(Singer, -TotalStreams, sum), y = TotalStreams, fill = Christmasartist)) +
+    geom_col() +
+    scale_y_continuous(labels = label_number(suffix = " Mil", scale = 1e-6)) +
+    scale_fill_manual(values = c("#811d1d", "#17503a")) +
+    labs(title = "Top 10 Streamed Artists", x = "Artist", y = "Total Streams", fill = "Christmas Artist")
+
+ggsave("documents/top10streamed.png", plot = last_plot())
+
+
